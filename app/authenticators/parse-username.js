ecdfa4b5313106cb0692a5a4a2b74090408788f9
Jacob Smith <jacob@jacobsmith.io>
2015-07-06 14:07:23 -0400

    Fix authenticator#restore

--- app/authenticators/parse-username.js
+++ app/authenticators/parse-username.js
@@ -1,17 +1,23 @@
+import Ember from 'ember';
 import BaseAuthenticator from 'simple-auth/authenticators/base';
 
 export default BaseAuthenticator.extend({
   restore: function(data) {
     data = data || {};
 
-    var adapter = this.container.lookup('store:main').adapterFor('parseUser');
+    if(data.sessionToken) {
+      var adapter = this.container.lookup('store:main').adapterFor('parseUser');
 
-    return adapter.ajax('https://api.parse.com/1/users/me', 'GET').then(function(response) {
-      return {
-        userData: response,
-        sessionToken: data.sessionToken
-      };
-    });
+      return adapter.ajax('https://api.parse.com/1/users/me', 'GET', {
+        headers: {
+          'X-Parse-Session-Token': data.sessionToken
+        }
+      }).then(function(response) {
+        return normalizeSessionData(response);
+      });
+    } else {
+      return Ember.RSVP.reject("Session token not found");
+    }
   },
 
   authenticate: function(data) {
@@ -25,10 +31,16 @@ export default BaseAuthenticator.extend({
         password: data.password
       }
     }).then(function(response) {
-      data = {sessionToken: response.sessionToken};
-      delete response.sessionToken;
-      data.userData = response;
-      return data;
+      return normalizeSessionData(response);
     });
   }
 });
+
+function normalizeSessionData(response) {
+  var data = {
+    sessionToken: response.sessionToken,
+    userData: response
+  };
+  delete data.userData.sessionToken;
+  return data;
+}
