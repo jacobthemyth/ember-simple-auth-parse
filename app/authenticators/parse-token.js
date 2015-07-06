ccd384545d4901d0a702842de56b61d1fd8dd11a
Jacob Smith <jacob@jacobsmith.io>
2015-07-06 15:45:48 -0400

    Implement parse-token authenticator

---
+++ app/authenticators/parse-token.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+import BaseAuthenticator from 'simple-auth/authenticators/base';
+
+export default BaseAuthenticator.extend({
+  restore: function(data) {
+    data = data || {};
+
+    if(data.sessionToken) {
+      var adapter = this.container.lookup('store:main').adapterFor('parseUser');
+
+      return adapter.ajax('https://api.parse.com/1/users/me', 'GET', {
+        headers: {
+          'X-Parse-Session-Token': data.sessionToken
+        }
+      }).then(function(response) {
+        return normalizeSessionData(response);
+      });
+    } else {
+      return Ember.RSVP.reject("Session token not found");
+    }
+  },
+
+  authenticate: function(data) {
+    data = data || {};
+
+    var adapter = this.container.lookup('store:main').adapterFor('parseUser');
+
+    return adapter.ajax('https://api.parse.com/1/users/me', 'GET', {
+      headers: {
+        'X-Parse-Session-Token': data.sessionToken
+      }
+    }).then(function(response) {
+      return normalizeSessionData(response);
+    });
+  }
+});
+
+function normalizeSessionData(response) {
+  var data = {
+    sessionToken: response.sessionToken,
+    userData: response
+  };
+  delete data.userData.sessionToken;
+  return data;
+}
